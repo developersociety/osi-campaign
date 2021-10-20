74a2dd3d26708796bd48aa000698e59abbb794d9
Martin Coote <martin@martincoote.com>
2021-10-20 15:18:54 +0100

    update member list functionality

--- static/js/components/member-list/member-list.js
+++ static/js/components/member-list/member-list.js
@@ -1,28 +1,44 @@
 class memberList extends HTMLElement {
   constructor() {
     super()
+    this.i = 0;
   }
 
   async connectedCallback() {
     // Get the dummy data
-    const members = await this.getMembers();
+    this.members = await this.getMembers();
+    this.members.reverse();
 
     // Get the template from a seperate html file
-    const template = await this.getTemplate();
+    this.template = await this.getTemplate();
 
     // Create a shadow dom for appending elements to
     this.attachShadow({mode: 'open'})
 
     // create the parent list element
-    const parentList = document.createElement('ul')
-    parentList.classList.add('member-list')
-
-    // Populate members
-    members.forEach(member => this.addListItem(member, template, parentList))
+    this.parentList = document.createElement('ul')
+    this.parentList.classList.add('member-list')
 
     // append the created elements to the shadow dom
-    this.shadowRoot.append(parentList);
+    this.shadowRoot.append(this.parentList);
+    this.addMembersLoop();
+  }
 
+  async addMembersLoop() {
+    // debugger;
+    // Populate members
+    const member = this.members[this.i];
+    if (this.i < 5) {
+      this.addListItem(member, this.template, this.parentList)
+      this.i++
+      this.addMembersLoop()
+    } else {
+    setTimeout(() => {
+        this.addListItem(member, this.template, this.parentList);
+        this.i++
+        this.addMembersLoop()
+      }, 3000)
+    }
   }
 
   async getMembers() {
@@ -49,16 +65,19 @@ class memberList extends HTMLElement {
     memberTemplate.querySelector('#message').innerText = `"${obj.message}"`
     memberTemplate.querySelector('#time-stamp').innerText = formattedTime
 
-    parentElement.appendChild(memberTemplate)
+    parentElement.prepend(memberTemplate)
     return
   }
 
   formatTimestamp(pastTime) {
+    const rtf = new Intl.RelativeTimeFormat('en');
     const now = new Date();
     const past = new Date(pastTime);
-    const diffTime = Math.abs(past - now);
+    const diffTime = Math.abs(now - past);
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-
+    const diffHours = Math.ceil(diffTime / (1000 * 60 * 60));
+    if (diffHours < 24)  return rtf.format(-diffHours, "hour")
+    return rtf.format(-diffDays, "day")
   }
 }
 
