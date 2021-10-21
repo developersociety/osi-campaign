5dd125ec6e42d0f3cb43334253bb9061b77a0168
Martin Coote <martin@martincoote.com>
2021-10-21 14:43:31 +0100

    WIP configure Zapier post

--- static/js/form.js
+++ static/js/form.js
@@ -1,8 +1,10 @@
 const form = document.getElementById('membership-form')
 const submitButton = form.querySelector('button[type=submit]')
 const nameInput = form.querySelector('input[name=name]')
+const emailInput = form.querySelector('input[name=email]')
+const countryInput = form.querySelector('select[name=country]')
 
-const makeStripePayment = () => {
+const makeStripePayment = async () => {
   // Replace with your own publishable key: https://dashboard.stripe.com/test/apikeys
   var PUBLISHABLE_KEY = 'pk_test_WCJLe4VIHyg3wdUQYvMaBa5K00z9hFMGJ2';
   // Replace with the domain you want your users to be redirected back to after payment
@@ -11,26 +13,47 @@ const makeStripePayment = () => {
   var stripe = Stripe(PUBLISHABLE_KEY);
 
   // Handle any errors from Checkout
-  var handleResult = function (result) {
+  var handleResult =  function (result) {
     if (result.error) {
       var displayError = document.getElementById('error-message');
       displayError.textContent = result.error.message;
     }
   };
 
-    // Make the call to Stripe.js to redirect to the checkout page
-    // with the sku or plan ID.
-    stripe
-      .redirectToCheckout({
-        mode: 'subscription',
-        lineItems: [{ price: 'price_1JjQ5LAL5YtKKJj5KS3lH6U9', quantity: 1 }],
-        successUrl:
-          DOMAIN + 'thank-you.html?session_id={CHECKOUT_SESSION_ID}' + `&name=${nameInput.value}`,
-        cancelUrl:
-          DOMAIN + 'index.html?status=cancelled'
-      })
-      .then(handleResult);
+  const data = {
+    name: nameInput.value,
+    email: emailInput.value,
+    country: countryInput.value,
+    membership: 'Individual',
+    'opt-in': true
   }
+
+  const zapier_submission = await fetch('https://hooks.zapier.com/hooks/catch/576272/bh4m1pl/', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  const response = await zapier_submission.json()
+
+  return console.log(response)
+
+  // Make the call to Stripe.js to redirect to the checkout page
+  // with the sku or plan ID.
+  stripe
+    .redirectToCheckout({
+      mode: 'subscription',
+      lineItems: [{ price: 'price_1JjQ5LAL5YtKKJj5KS3lH6U9', quantity: 1 }],
+      successUrl:
+        DOMAIN + 'thank-you.html?session_id={CHECKOUT_SESSION_ID}' + `&name=${nameInput.value}`,
+      cancelUrl:
+        DOMAIN + 'index.html?status=cancelled'
+    })
+    .then(handleResult);
+}
 
 
 const handleSubmit = (event) => {
